edf5bf815810a1dac6c89b3a388eaff95cc0f84d
Philipp Muens <raute1337@gmx.de>
2015-10-05 14:34:01 +0200

    Add allow rules based validation
    Add allow rules based validation so that all kind of insert,
    update and remove actions are checked and not only those
    performed with the help of the form.

--- client/views/form.js
+++ client/views/form.js
@@ -7,15 +7,13 @@ Template.form.events({
       body: template.find('form textarea').value
     };
 
-    if (note.body.length !== 0) {
-      if (note._id.length === 0) {
-        Notes.insert({ body: note.body, modifiedAt: new Date() });
-      } else {
-        Notes.update({ _id : note._id }, { $set: { body: note.body, modifiedAt: new Date() } });
-      }
-      template.find('form textarea').value = '';
-      template.find('form input[type="hidden"]').value = '';
+    if (note._id.length === 0) {
+      Notes.insert({ body: note.body, modifiedAt: new Date() });
+    } else {
+      Notes.update({ _id : note._id }, { $set: { body: note.body, modifiedAt: new Date() } });
     }
+    template.find('form textarea').value = '';
+    template.find('form input[type="hidden"]').value = '';
   },
 
   'click button': function (event, template) {
--- collections/notes.js
+++ collections/notes.js
@@ -1,1 +1,19 @@
 Notes = new Mongo.Collection('notes');
+
+Notes.allow({
+  insert: (userId, document) => {
+    if (document.body.length !== 0) {
+      return true;
+    }
+  },
+
+  update: (userId, document, fieldNames, modifier) => {
+    if (modifier['$set'].body.length !== 0) {
+      return true;
+    }
+  },
+
+  remove: (userId, document) => {
+    return true;
+  }
+});
